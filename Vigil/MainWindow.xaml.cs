ca3cae814ea29fd4a48591a099ade51650078cd8
Matt <armada2526pbem@gmail.com>
2020-11-09 13:41:36 -0700

    GET request functions, JSON properly deserializes
    Assuming textboxes are all correctly filled, with correct information (there's no "dummy proofing"), a GET request is sent to the server. The response is received and deserialized.
    
    MessageBoxes show up throughout the process, and Label_MainDisplay is not used - yet.

--- Vigil/MainWindow.xaml.cs
+++ Vigil/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,63 @@ namespace Vigil
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Strings to hold input from TextBoxes
+        string serverAddress;
+        string familyName;
+        string deviceName;
+
+        // Dictionary holding FIND3 API calls
+        // A function would likely work better, but for proof of concept, this should work.
+        readonly Dictionary<string, string> find3ApiCalls = new Dictionary<string, string>()
+        {
+            { "simpleLocationOfSingleDevice", "/api/v1/location_basic/" }, // after appending /FAMILY/DEVICE gets location of device (loc), probability (p), and last seen in seconds (seen). Also gets GPS (lat) (lon)
+        };
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+            
+        }
+
+        // Fires when Button_GetDeviceLocation is clicked
+        private void Button_GetDeviceLocation_Click(object sender, RoutedEventArgs e)
+        {
+            SimpleDevice simpleDevice;
+            serverAddress = TextBox_ServerAddress.Text;
+            familyName = TextBox_FamilyName.Text;
+            deviceName = TextBox_DeviceName.Text;
+            string response;
+            
+
+            string uri = serverAddress + find3ApiCalls["simpleLocationOfSingleDevice"] + familyName + "/" + deviceName;
+
+            MessageBox.Show("URI built: " + uri);
+
+            response = @Get(uri);
+            MessageBox.Show( "GET response: " + response);
+            MessageBox.Show("deserializing");
+
+            simpleDevice = JsonSerializer.Deserialize<SimpleDevice>(response);
+            MessageBox.Show("message: " + simpleDevice.message);
+            MessageBox.Show("location: " + simpleDevice.data.loc);
+
+
+
+
+        }
+
+        public string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
---
+++ Vigil/SimpleDevice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vigil
+{
+    // this file contains classes necessary to handle the response to a Get simple location of a single device from a FIND3 server.
+
+    // Overall response from server after GETing simpleDeviceLocation
+    public class SimpleDevice
+    {
+        // "Data" section of response -- see below for class
+        public data data { get; set; }
+        // Message - e.g., "ok"
+        public string message { get; set; }
+        // Success - e.g., true
+        public bool success { get; set; }
+    }
+
+    // "Data" section of response
+    public class data
+    {
+        // Location - e.g., "kitchen" -- the server's current guess as to the device's location
+        public string loc { get; set; }
+        // GPS - e.g., gps[lat] -> 47.5675768678. Both = -1 if no GPS coordinates available.
+        Dictionary<string, double> gps { get; set; }
+        // Probability - e.g., 0.97 (=97%) -- how confident the server is that the device is at current location
+        public float prob { get; set; }
+        // [last] Seen - e.g., 1387 -- how many seconds since the device was last seen by the server
+        public int seen { get; set; }
+
+    }
+}
