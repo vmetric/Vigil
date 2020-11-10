a0b898bab3443d2ef332798801390e8e3d1081c9
Matt <armada2526pbem@gmail.com>
2020-11-09 21:58:32 -0700

    Complete TODO line 45; add http:// to serverAddress if it begins with a number (i.e., is an ip address); complain and return if not an IP address and does not begin with http:// or https://
    New TODO on line 53 in MainWindow.xaml.cs
    
    Turns out you can center a Label's content both horizontally and vertically, AND that you cannot vertically center a TextBlock's content. So, need to replace the TextBlock with a Label so we can use multi-line outputs. OR, put TextBlock inside of a label so MainDisplay is fully centered, while also keeping textwrapping.
    label: https://i.imgur.com/2utdDYI.jpg

--- Vigil/MainWindow.xaml.cs
+++ Vigil/MainWindow.xaml.cs
@@ -37,16 +37,43 @@ namespace Vigil
             TextBlock_MainDisplay.Text = "Working...";
             
             SimpleLocationOfDevice simpleDevice;
-
+            
+            string uri = "";
             serverAddress = TextBox_ServerAddress.Text;
+
+            // If the first character of serverAddress is a number, it's safe to assume it's an IP address
+            // And has been entered without http:// or https:// -- thus, we can add http:// to the front.
+            // TODO verify if a GET request (below in Get()) will properly redirect from Http to Https
+            if (char.IsNumber(serverAddress[0]))
+            {
+                serverAddress = serverAddress.Insert(0, "http://");
+            } else if (!serverAddress.StartsWith("http://") || !serverAddress.StartsWith("https://")) // else, it does not begin with a number, so it must be a domain name. If it does not begin w/ http or https...
+            {
+                // Complain to user
+                // TODO replace TextBlock with Label, and complain via MainDisplay. (Or, maybe, put TextBlock inside of label to acheive full content centering, while maintaining textwrapping?)
+                MessageBox.Show("URLs must begin with http:// or https:// \nFor example: https://google.com");
+                // Return, we don't want a broken URL
+                return;
+                // TODO maybe better if we correct the URL anyway, then get a confirmation from the user that the address is correct? This could be a "settings" option for user to check/uncheck.
+            }
+
             familyName = TextBox_FamilyName.Text;
             deviceName = TextBox_DeviceName.Text;
-            
-            //TODO check if trailing / exists in serverAddress before adding
-            string uri = serverAddress + "/" + find3ApiCalls["simpleLocationOfSingleDevice"] + familyName + "/" + deviceName;
 
+            // Start building uri by adding serverAddress
+            uri += serverAddress;
+            // If serverAddress does NOT end with a /, add it to uri
+            if (!serverAddress.EndsWith('/'))
+            {
+                uri += "/";
+            }
+            // Add the simpleLocationOfSingleDevice API call location, family name, slash, and deviceName.
+            uri += find3ApiCalls["simpleLocationOfSingleDevice"] + familyName + "/" + deviceName;
+
+            // GET from server, Deserialize JSON data (that is, convert it into a class simpleDevice)
             simpleDevice = JsonSerializer.Deserialize<SimpleLocationOfDevice>(await @Get(uri));
 
+            // Update MainDisplay with given device location.
             TextBlock_MainDisplay.Text = $"Location Acquired: {simpleDevice.data.loc}";
 
         }
